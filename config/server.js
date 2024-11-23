89ad2f69e201162dd808f6b3c7a61a18ae0828a6
Aviral Shukla <aviralshukla@Avirals-MacBook-Air.local>
2024-11-23 14:37:52 +0530

    POrt

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '127.0.0.1'),
-  port: env.int('PORT', 1337),
+  host: env('HOST'),
+  port: env.int('PORT'),
   app: {
     keys: env.array('APP_KEYS'),
   },
